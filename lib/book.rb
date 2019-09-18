6a796d30d57325d68fd8462acc52f65ec435a229
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-18 02:02:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,5 +10,21 @@ class Book
   def author=(author)
     @author = author
   end
+  
+  def author
+    @author
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
 end 
 
